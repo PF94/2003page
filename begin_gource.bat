35dcbeecab90851e3b97fa48b1e4a6603befe1f3
ChazizPF94 <45898787+PF94@users.noreply.github.com>
2020-03-30 12:12:55 -0400

    moved begin_gource files to my PC only

--- begin_gource.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo By running this command, you agree to not to modify the file without PF94's permission
-pause
-
-gource -s 0.75 --title 2003page.ga --background-colour 322657 -a 0.0001 -key --max-file-lag 0.001
