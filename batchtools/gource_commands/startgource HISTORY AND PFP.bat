b1983dc5767253c92afc4ec4fe471a69d146b3fc
ChazizPF94 <45898787+PF94@users.noreply.github.com>
2020-10-15 15:03:54 -0400

    yes

--- batchtools/gource_commands/startgource HISTORY AND PFP.bat
+++ batchtools/gource_commands/startgource HISTORY AND PFP.bat
@@ -1,3 +1,3 @@
-gource --load-config config.txt PROPER_GOURCE_HISTORY.txt  --user-image-dir PFPs
+gource --load-config config.txt PROPER_GOURCE_HISTORY.txt  --user-image-dir PFPs --viewport 1280x720
 pause
 pause
